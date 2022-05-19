4e4514c6b3a985afb6fe40f7a6da15889ebc9800
rmadrigal10WST <rmadrigal@ahorra.io>
2022-05-19 11:26:45 -0500

    apiAhorra

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getUsers } = require('../controllers/index.controller');
+const { getUsers, getUserById, createUser, deleteUser, updateUser } = require('../controllers/index.controller');
 
 router.get('/users', getUsers);
+router.get('/users/:id', getUserById);
+router.post('/users', createUser);
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
+
+
 
 module.exports = router;
