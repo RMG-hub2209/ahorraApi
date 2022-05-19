2c8a4edd2af66d7368d8da9dcbe1105fb1c9baa5
rmadrigal10WST <rmadrigal@ahorra.io>
2022-05-19 13:38:10 -0500

    cors enabled

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+
 const port = 3000;
 
 //middlewares
-app.use(express.json());
+app.use(express.json(), cors());
 
 
 //routes
 app.use(require('./routes/index'));
+
+app.get('/users/:id', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'});
+  });
+app.listen(80, function () {
+    console.log('CORS-enabled web server listening on port 80');
+  });
 
 app.listen(port, () => {
     console.log('En el puerto: ' + port);
